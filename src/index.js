15997dedc8824f17c004c18eccf036d16d29d3f9
Mara Hart <maralihart@gmail.com>
2021-06-10 15:29:10 -0700

    added diff

--- src/index.js
+++ src/index.js
@@ -5,39 +5,23 @@ async function autoMerge() {
   try {
     const merge_method = core.getInput("merge-method");
     const payload = github.context.payload;
-
-    core.info("---PAYLOAD---")
-    core.info(JSON.stringify(payload));
     
     const myToken = core.getInput("github-token");
     const octokit = github.getOctokit(myToken);
-    core.info("---OCTOKIT---")
-    core.info(myToken.length);
-    core.info(JSON.stringify(octokit));
 
-    core.info("PR #: ", payload.issue.number)
     const owner = payload.issue.user.login;
     const repo = payload.repository.name;
     const pr_number = payload.issue.number;
 
-    core.info("owner: ")
-    core.info(owner)
-    core.info("repo: ")
-    core.info(repo)
-    core.info("pr_number: ")
-    core.info(pr_number);
-
-    const allPullRequests = await octokit.rest.pulls.list({
-      owner,
-      repo,
-    });
-    
-    core.info(JSON.stringify(allPullRequests));
-
-    const { data } = allPullRequests;
-    core.info(data);
-
     // TODO: check to make sure only one line has been changed!
+    const diff = await octokit.rest.pulls.list({
+      owner: owner,
+      repo: repo,
+      pull_number: pr_number
+    }).data[0].diff_url;
+
+    core.info("diff");
+    core.info(diff);
 
     // TODO: merge 
 
@@ -45,7 +29,7 @@ async function autoMerge() {
       owner: owner,
       repo: repo,
       pull_number: pr_number,
-      merge_method: "merge",
+      merge_method: "merge"
     });
 
     core.info("PR successfully merged!");
