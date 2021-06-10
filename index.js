dd7110b9c77ee6b3490fe8d28268d71dca008d6b
Mara Hart <maralihart@gmail.com>
2021-06-10 16:38:07 -0700

    added comment and webscraping

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
+const axios = require("axios");
+const cheerio = require("cheerio");
+
 async function autoMerge() {
   try {
     const payload = github.context.payload;
@@ -25,7 +28,14 @@ async function autoMerge() {
     const oneLineAdded = additions === 1 && deletions === 0;
 
     // TODO: take care of merge conflicts?
-    if (!mergeable) {
+    if (!mergeable && oneLineAdded) {
+      const diffURL = pr.data.diff_url;
+      const diff = await getDiff(diffURL);
+      core.info("diff");
+      core.info(diff);
+      // make a new copy?
+      // delete old pr
+      // call the commit a version of the person's name?
       core.info("can't merge, oop")
     }
 
@@ -37,12 +47,43 @@ async function autoMerge() {
         merge_method: "merge"
       });
 
+      if (!oneLineAdded) {
+        // TODO: add a comment to the PR saying that only one line can be changed
+        octokit.rest.pulls.createReviewComment({
+          owner: owner,
+          repo: repo,
+          pull_number: prNumber,
+          body: "Try changing your code so you're only adding your hometown, then ask someone else to comment again for it to automatically merge!",
+        });
+      }
+
       core.info("PR successfully merged!");
     }
 
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+async function getDiff(url) {
+  const regex = /\+[a-zA-Z]+[\s\S]*/gm;
+
+  const { data } = await axios.get(url);
+  const $ = cheerio.load(data);
+  const txt = $('pre'.text());
+
+  let search;
+  let diff;
+  while ((search = regex.exec(str)) !== null) {
+    if (search.index === regex.lastIndex) regex.lastIndex++;
+
+    search.forEach((match, groupIndex) => {
+      diff = match;
+    });
+
+  return diff
+}
+
 }
 
 autoMerge();
