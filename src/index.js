576b618fe3eb54349415901e0ff5364fc76b683e
Mara Hart <maralihart@gmail.com>
2021-06-09 21:36:19 -0700

    refactored

--- src/index.js
+++ src/index.js
@@ -1,41 +1,34 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const { Toolkit } = require("actions-toolkit");
-const tools = new Toolkit();
 
 async function autoMerge() {
   try {
     const merge_method = core.getInput("merge-method");
     const payload = github.context.payload;
-    core.info(payload);
-    core.info(payload.comment);
-    core.info(payload.pull_request.title);
+
+    core.info("---PAYLOAD---")
+    core.info(JSON.stringify(payload));
     
     const myToken = core.getInput("github-token");
-    const pulls = github.getOctokit(myToken).rest.pulls;
+    const octokit = github.getOctokit(myToken);
 
-    const ref = tools.context.ref;
-    const pull_number = Number(ref.split("/")[2]);
-    core.info("PR #: ", pull_number)
+    core.info("PR #: ", payload.pull_request_number)
     
-    core.info(pulls)
+    core.info(JSON.stringify(octokit.pulls));
 
-    const pr = await pulls.get({
-      ...github.context.repo,
-      pull_number,
+    await octokit.pulls.merge({
+      owner: payload.repository.owner.login,
+      repo: payload.repository.name,
+      pull_number: payload.pull_request_number,
+      merge_method: "merge",
     });
 
-    pulls.merge({
-      ...github.context.repo,
-      pull_number,
-      merge_method,
-    });
     core.info(`Success Merge PR!`);
 
-    core.info("The Bot will remove merged branch.");
     octokit.git.deleteRef({
-      ...github.context.repo,
-      ref,
+      owner: payload.repository.owner.login,
+      repo: payload.repository.name,
+      ref: payload.pull_request.head.sha
     });
   } catch (error) {
     core.setFailed(error.message);
