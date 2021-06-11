88158c386f12960a0331bd3744201a8a9ec6cfc0
Mara Hart <maralihart@gmail.com>
2021-06-10 22:51:43 -0700

    added so much for conflicts

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-
+const { Base64 } = require("js-base64");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const fs = require("fs");
 
 async function autoMerge() {
   try {
     const payload = github.context.payload;
-    
+    const filepath = core.getInput("filepath");
+    const raw_link = core.getInput("raw_link");
+    const email = core.getInput("email");
     const myToken = core.getInput("github-token");
     const octokit = github.getOctokit(myToken);
 
@@ -28,14 +31,6 @@ async function autoMerge() {
     const oneLineAdded = additions === 1 && deletions === 0;
 
     if (!oneLineAdded) {
-      // TODO: add a comment to the PR saying that only one line can be changed
-      // await octokit.rest.pulls.createReviewComment({
-      //   owner: owner,
-      //   repo: repo,
-      //   pull_number: prNumber,
-      //   position: 1,
-      //   body: "Try changing your code so you're only adding your hometown, then ask someone else to comment again for it to automatically merge!",
-      // });
       core.info("Too many lines were changed. PR cannot be merged");
       return;
     };
@@ -51,12 +46,24 @@ async function autoMerge() {
       core.info(diff);
       core.info("-----------");
 
-      
+      const content = await buildFile(raw_link, diff);
+      const contentEncoded = Base64.encode(content);
 
+      await octokit.repos.createOrUpdateFileContents({
+        repo: repo,
+        path: filepath,
+        message: `Update location.txt with ${owner}'s hometown`,
+        content: contentEncoded,
+        committer: {
+          name: "GitHub-Actions",
+          email: email,
+        },
+        author: {
+          name: owner,
+          email: email,
+        }
+      })
 
-      // make a new copy?
-      // delete old pr
-      // call the commit a version of the person's name?
       core.info("Cannot automatically merge this branch");
       return;
     };
@@ -87,6 +94,25 @@ async function getDiff(url) {
     search.forEach((match, groupIndex) => {diff = match;});
   }
   return diff;
+}
+
+async function buildFile(url, addition) {
+  let search, diff;
+  const regex = /<pre[\S\s]+>([\S.]*)<\/pre>/gm;
+  const { data } = await axios.get(url);
+  const html = cheerio.load(data).html();
+  core.info("html");
+  core.info(html);
+  core.info("----");
+  while ((search = regex.exec(html)) !== null) {
+    if (search.index === regex.lastIndex) regex.lastIndex++;
+    search.forEach((match, groupIndex) => {
+      core.info("match");
+      core.info(match);
+      core.info("----")
+      diff = match;
+    });
+  }
 }
 
 autoMerge();
