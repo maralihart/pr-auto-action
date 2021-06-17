bd5115f104cae49f5e92161a8b41e2d224a705e9
Mara Hart <maralihart@gmail.com>
2021-06-17 09:59:03 -0700

    added owner

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ async function autoMerge() {
 
     core.info(JSON.stringify(pr))
     core.setOutput("ref", pr.data.head.ref);
+    core.setOutput("owner", owner);
     const mergeable = pr.data.mergeable_state;
     const onlyOneChangedFile = pr.data.changed_files === 1;
     const additions = pr.data.additions;
