94174ff5c72cbcf567bb3c05758282d1c83ae070
Mara Hart <maralihart@gmail.com>
2021-06-21 18:41:53 -0700

    works for just one line added, no conflicts

--- copy.js
+++ copy.js
@@ -1,464 +1,185 @@
-import * as core from '@actions/core'
-import path from 'path'
-import simpleGit, { Response } from 'simple-git'
-import YAML from 'js-yaml'
-import {
-  getInput,
-  getUserInfo,
-  Input,
-  log,
-  matchGitArgs,
-  outputs,
-  readJSON,
-  setOutput
-} from './util'
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { Base64 } = require("js-base64");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-const baseDir = path.join(process.cwd(), getInput('cwd') || '')
-const git = simpleGit({ baseDir })
-core.info(`Running in ${baseDir}`)
-;(async () => {
-  await checkInputs().catch(core.setFailed)
+async function autoMerge() {
+  try {
+    const payload = github.context.payload;
+    const filepath = core.getInput("filepath");
+    const raw_link = core.getInput("raw_link");
+    const email = core.getInput("email");
+    const myToken = core.getInput("github-token");
+    const octokit = github.getOctokit(myToken).plugin(require("octokit-commit-multiple-files"));
 
-  core.startGroup('Internal logs')
-  core.info('> Staging files...')
+    const owner = payload.issue.user.login;
+    const repo = payload.repository.name;
+    const prNumber = payload.issue.number;
 
-  if (getInput('add')) {
-    core.info('> Adding files...')
-    await add()
-  } else core.info('> No files to add.')
+    const pr = await octokit.rest.pulls.get({
+      owner: owner,
+      repo: repo,
+      pull_number: prNumber
+    });
 
-  if (getInput('remove')) {
-    core.info('> Removing files...')
-    await remove()
-  } else core.info('> No files to remove.')
+    core.info(JSON.stringify(pr))
+    core.setOutput("ref", pr.data.head.ref);
+    core.setOutput("owner", owner);
+    const sha = pr.data.base.sha;
+    const mergeable = pr.data.mergeable_state;
+    const onlyOneChangedFile = pr.data.changed_files === 1;
+    const additions = pr.data.additions;
+    const deletions = pr.data.deletions;
+    const oneLineAdded = additions === 1 && deletions === 0;
 
-  core.info('> Checking for uncommitted changes in the git working tree...')
-  const changedFiles = (await git.diffSummary(['--cached'])).files.length
-  if (changedFiles > 0) {
-    core.info(`> Found ${changedFiles} changed files.`)
+    if (!oneLineAdded) {
+      core.info("Too many lines were changed. PR cannot be merged");
+      return;
+    };
 
-    await git
-      .addConfig('user.email', getInput('author_email'), undefined, log)
-      .addConfig('user.name', getInput('author_name'), undefined, log)
-    core.debug(
-      '> Current git config\n' +
-        JSON.stringify((await git.listConfig()).all, null, 2)
-    )
+    // TODO: Fix "dirty" PRs
 
-    await git.fetch(['--tags', '--force'], log)
+    if (mergeable == "dirty") {
+      // TODO: take care of merge conflicts?
+      core.info("DIRTY")
+      dirty = true;
+      core.setOutput("dirty", dirty);
+      const diffURL = pr.data.diff_url;
+      const diff = await getDiff(diffURL);
 
-    core.info('> Switching/creating branch...')
-    await git
-      .checkout(getInput('branch'), undefined, log)
-      .catch(() => git.checkoutLocalBranch(getInput('branch'), log))
+      core.info(diff);
+      core.setOutput("diff", diff);
 
-    if (getInput('pull_strategy') == 'NO-PULL')
-      core.info('> Not pulling from repo.')
-    else {
-      core.info('> Pulling from remote...')
-      await git.fetch(undefined, log).pull(
-        undefined,
-        undefined,
-        {
-          [getInput('pull_strategy')]: null
-        },
-        log
-      )
-    }
+      core.info("BUILD FILE")
+      const content = await buildFile(raw_link, diff);
+      core.info(content);
+      core.info("CONTENT CREATED")
+      const contentEncoded = Base64.encode(content);
+      core.info("CONTENT ENCODED")
 
-    core.info('> Re-staging files...')
-    if (getInput('add')) await add({ ignoreErrors: true })
-    if (getInput('remove')) await remove({ ignoreErrors: true })
+      // TODO: Error: Cannot read property 'createOrUpdateFileContents' of undefined
+      core.info("REST")
+      const allRepos = await octokit.rest.repos
+      core.info(JSON.stringify(allRepos))
+      const { data } = allRepos
+      core.info(JSON.stringify(data))
+      core.info("---")
 
-    core.info('> Creating commit...')
-    await git.commit(
-      getInput('message'),
-      undefined,
-      {
-        '--author': `"${getInput('author_name')} <${getInput(
-          'author_email'
-        )}>"`,
-        ...(getInput('signoff')
-          ? {
-              '--signoff': null
-            }
-          : {})
-      },
-      (err, data?) => {
-        if (data) setOutput('committed', 'true')
-        return log(err, data)
-      }
-    )
+      
 
-    if (getInput('tag')) {
-      core.info('> Tagging commit...')
-      await git
-        .tag(matchGitArgs(getInput('tag')), (err, data?) => {
-          if (data) setOutput('tagged', 'true')
-          return log(err, data)
-        })
-        .then((data) => {
-          setOutput('tagged', 'true')
-          return log(null, data)
-        })
-        .catch((err) => core.setFailed(err))
-    } else core.info('> No tag info provided.')
-
-    let pushOption: string | boolean
-    try {
-      pushOption = getInput('push', true)
-    } catch {
-      pushOption = getInput('push')
-    }
-    if (pushOption) {
-      // If the options is `true | string`...
-      core.info('> Pushing commit to repo...')
-
-      if (pushOption === true) {
-        core.debug(
-          `Running: git push origin ${getInput('branch')} --set-upstream`
-        )
-        await git.push(
-          'origin',
-          getInput('branch'),
-          { '--set-upstream': null },
-          (err, data?) => {
-            if (data) setOutput('pushed', 'true')
-            return log(err, data)
-          }
-        )
-      } else {
-        core.debug(`Running: git push ${pushOption}`)
-        await git.push(
-          undefined,
-          undefined,
-          matchGitArgs(pushOption),
-          (err, data?) => {
-            if (data) setOutput('pushed', 'true')
-            return log(err, data)
-          }
-        )
-      }
-
-      if (getInput('tag')) {
-        core.info('> Pushing tags to repo...')
-        await git
-          .pushTags('origin', undefined, (e, d?) => log(undefined, e || d))
-          .catch(() => {
-            core.info(
-              '> Tag push failed: deleting remote tag and re-pushing...'
-            )
-            return git
-              .push(
-                undefined,
-                undefined,
-                {
-                  '--delete': null,
-                  origin: null,
-                  [matchGitArgs(getInput('tag')).filter(
-                    (w) => !w.startsWith('-')
-                  )[0]]: null
+      try {
+        // await octokit.rest.repos.createOrUpdateFileContents({
+        //   repo: repo,
+        //   path: filepath,
+        //   message: `Update location.txt with ${owner}'s hometown`,
+        //   content: contentEncoded,
+        //   committer: {
+        //     name: "GitHub-Actions",
+        //     email: email,
+        //   },
+        //   author: {
+        //     name: owner,
+        //     email: email,
+        //   }
+        // })
+        // await octokit.request('PUT /repos/maralihart/test-repo/contents/test.txt', {
+        //   owner: owner,
+        //   repo: repo,
+        //   path: 'test.txt',
+        //   message: 'solving dirty pr',
+        //   content: contentEncoded,
+        //   sha: sha,
+        // })
+        const branchName = await octokit.repos.createOrUpdateFiles({
+          owner: "maralihart",
+          repo: "test-repo",
+          branch: "main",
+          createBranch: true,
+          changes: [
+            {
+              message: "Your commit message",
+              files: {
+                "test.md": `# This is a test`,
+                "test2.md": {
+                  contents: `Something else`,
                 },
-                log
-              )
-              .pushTags('origin', undefined, log)
-          })
-      } else core.info('> No tags to push.')
-    } else core.info('> Not pushing anything.')
-
-    core.endGroup()
-    core.info('> Task completed.')
-  } else {
-    core.endGroup()
-    core.info('> Working tree clean. Nothing to commit.')
-  }
-})()
-  .then(logOutputs)
-  .catch((e) => {
-    core.endGroup()
-    logOutputs()
-    core.setFailed(e)
-  })
-
-async function checkInputs() {
-  function setInput(input: Input, value: string | undefined) {
-    if (value) return (process.env[`INPUT_${input.toUpperCase()}`] = value)
-    else return delete process.env[`INPUT_${input.toUpperCase()}`]
-  }
-  function setDefault(input: Input, value: string) {
-    if (!getInput(input)) setInput(input, value)
-    return getInput(input)
-  }
-
-  const eventPath = process.env.GITHUB_EVENT_PATH,
-    event = eventPath && readJSON(eventPath)
-
-  const isPR = process.env.GITHUB_EVENT_NAME?.includes('pull_request'),
-    defaultBranch = isPR
-      ? (event?.pull_request?.head?.ref as string)
-      : process.env.GITHUB_REF?.substring(11)
-
-  // #region add, remove
-  if (!getInput('add') && !getInput('remove'))
-    throw new Error(
-      "Both 'add' and 'remove' are empty, the action has nothing to do."
-    )
-
-  if (getInput('add')) {
-    const parsed = parseInputArray(getInput('add'))
-    if (parsed.length == 1)
-      core.info('Add input parsed as single string, running 1 git add command.')
-    else if (parsed.length > 1)
-      core.info(
-        `Add input parsed as string array, running ${parsed.length} git add commands.`
-      )
-    else core.setFailed('Add input: array length < 1')
-  }
-  if (getInput('remove')) {
-    const parsed = parseInputArray(getInput('remove'))
-    if (parsed.length == 1)
-      core.info(
-        'Remove input parsed as single string, running 1 git rm command.'
-      )
-    else if (parsed.length > 1)
-      core.info(
-        `Remove input parsed as string array, running ${parsed.length} git rm commands.`
-      )
-    else core.setFailed('Remove input: array length < 1')
-  }
-  // #endregion
-
-  // #region default_author
-  const default_author_valid = ['github_actor', 'user_info', 'github_actions']
-  if (!default_author_valid.includes(getInput('default_author')))
-    throw new Error(
-      `'${getInput(
-        'default_author'
-      )}' is not a valid value for default_author. Valid values: ${default_author_valid.join(
-        ', '
-      )}`
-    )
-
-  // #region author_name, author_email
-  let name, email
-  switch (getInput('default_author')) {
-    case 'github_actor': {
-      name = process.env.GITHUB_ACTOR
-      email = `${process.env.GITHUB_ACTOR}@users.noreply.github.com`
-      break
-    }
-
-    case 'user_info': {
-      if (!getInput('author_name') || !getInput('author_email')) {
-        const res = await getUserInfo(process.env.GITHUB_ACTOR)
-        if (!res?.name)
-          core.warning("Couldn't fetch author name, filling with github_actor.")
-        if (!res?.email)
-          core.warning(
-            "Couldn't fetch author email, filling with github_actor."
-          )
-
-        res?.name && (name = res?.name)
-        res?.email && (email = res.email)
-        if (name && email) break
+              },
+            },
+          ],
+        });
+        core.info("Successfully updated file");
+      } catch (error) {
+        core.setFailed(error.message);
       }
+      
+      core.info("Cannot automatically merge this branch");
+      return;
+    };
 
-      !name && (name = process.env.GITHUB_ACTOR)
-      !email && (email = `${process.env.GITHUB_ACTOR}@users.noreply.github.com`)
-      break
+    if (onlyOneChangedFile && oneLineAdded) {
+      try {
+        await octokit.rest.pulls.merge({
+          owner: owner,
+          repo: repo,
+          pull_number: prNumber,
+          merge_method: "merge"
+        });
+        core.info("PR successfully merged!");
+      } catch (error) {
+        core.info(error);
+      }
     }
 
-    case 'github_actions': {
-      name = 'github-actions'
-      email = '41898282+github-actions[bot]@users.noreply.github.com'
-      break
-    }
-
-    default:
-      throw new Error(
-        'This should not happen, please contact the author of this action. (checkInputs.author)'
-      )
+  } catch (error) {
+    core.setFailed(error.message);
   }
-
-  setDefault('author_name', name)
-  setDefault('author_email', email)
-  core.info(
-    `> Using '${getInput('author_name')} <${getInput(
-      'author_email'
-    )}>' as author.`
-  )
-  // #endregion
-
-  // #region message
-  setDefault(
-    'message',
-    `Commit from GitHub Actions (${process.env.GITHUB_WORKFLOW})`
-  )
-  core.info(`> Using "${getInput('message')}" as commit message.`)
-  // #endregion
-
-  // #region branch
-  const branch = setDefault('branch', defaultBranch || '')
-  if (isPR)
-    core.info(`> Running for a PR, the action will use '${branch}' as ref.`)
-  // #endregion
-
-  // #region signoff
-  if (getInput('signoff')) {
-    const parsed = getInput('signoff', true)
-
-    if (parsed === undefined)
-      throw new Error(
-        `"${getInput(
-          'signoff'
-        )}" is not a valid value for the 'signoff' input: only "true" and "false" are allowed.`
-      )
-
-    if (!parsed) setInput('signoff', undefined)
-
-    core.debug(
-      `Current signoff option: ${getInput('signoff')} (${typeof getInput(
-        'signoff'
-      )})`
-    )
-  }
-  // #endregion
-
-  // #region pull_strategy
-  if (getInput('pull_strategy') == 'NO-PULL')
-    core.debug("NO-PULL found: won't pull from remote.")
-  // #endregion
-
-  // #region push
-  if (getInput('push')) {
-    // It has to be either 'true', 'false', or any other string (use as arguments)
-    let value: string | boolean
-
-    try {
-      value = getInput('push', true)
-    } catch {
-      value = getInput('push')
-    }
-
-    core.debug(`Current push option: '${value}' (parsed as ${typeof value})`)
-  }
-  // #endregion
-
-  // #region github_token
-  if (!getInput('github_token'))
-    core.warning(
-      'No github_token has been detected, the action may fail if it needs to use the API'
-    )
 }
 
-async function add({ logWarning = true, ignoreErrors = false } = {}): Promise<
-  (void | Response<void>)[]
-> {
-  const input = getInput('add')
-  if (!input) return []
-
-  const parsed = parseInputArray(input)
-  const res: (void | Response<void>)[] = []
-
-  for (const args of parsed) {
-    res.push(
-      // Push the result of every git command (which are executed in order) to the array
-      // If any of them fails, the whole function will return a Promise rejection
-      await git
-        .add(matchGitArgs(args), (err: any, data?: any) =>
-          log(ignoreErrors ? null : err, data)
-        )
-        .catch((e: Error) => {
-          if (ignoreErrors) return
-          if (
-            e.message.includes('fatal: pathspec') &&
-            e.message.includes('did not match any files') &&
-            logWarning
-          )
-            core.warning(
-              `Add command did not match any file:\n  git add ${args}`
-            )
-          else throw e
-        })
-    )
-  }
-
-  return res
-}
-
-async function remove({
-  logWarning = true,
-  ignoreErrors = false
-} = {}): Promise<(void | Response<void>)[]> {
-  const input = getInput('remove')
-  if (!input) return []
-
-  const parsed = parseInputArray(input)
-  const res: (void | Response<void>)[] = []
-
-  for (const args of parsed) {
-    res.push(
-      // Push the result of every git command (which are executed in order) to the array
-      // If any of them fails, the whole function will return a Promise rejection
-      await git
-        .rm(matchGitArgs(args), (e: any, d?: any) =>
-          log(ignoreErrors ? null : e, d)
-        )
-        .catch((e: Error) => {
-          if (ignoreErrors) return
-          if (
-            e.message.includes('fatal: pathspec') &&
-            e.message.includes('did not match any files')
-          )
-            logWarning &&
-              core.warning(
-                `Remove command did not match any file:\n  git rm ${args}`
-              )
-          else throw e
-        })
-    )
-  }
-
-  return res
-}
-
-/**
- * Tries to parse a JSON array, then a YAML array.
- * If both fail, it returns an array containing the input value as its only element
- */
-function parseInputArray(input: string): string[] {
+async function getDiff(url) {
+  let search, diff;
+  const regex = /\+([a-zA-Z]+.*)/gm;
   try {
-    const json = JSON.parse(input)
-    if (
-      json &&
-      Array.isArray(json) &&
-      json.every((e) => typeof e == 'string')
-    ) {
-      core.debug(`Input parsed as JSON array of length ${json.length}`)
-      return json
+    const { data } = await axios.get(url);
+    const html = cheerio.load(data).html();
+    while ((search = regex.exec(html)) !== null) {
+      if (search.index === regex.lastIndex) regex.lastIndex++;
+      search.forEach((match, groupIndex) => {diff = match;});
     }
-  } catch {}
-
-  try {
-    const yaml = YAML.load(input)
-    if (
-      yaml &&
-      Array.isArray(yaml) &&
-      yaml.every((e) => typeof e == 'string')
-    ) {
-      core.debug(`Input parsed as YAML array of length ${yaml.length}`)
-      return yaml
-    }
-  } catch {}
-
-  core.debug('Input parsed as single string')
-  return [input]
-}
-
-function logOutputs() {
-  core.startGroup('Outputs')
-  for (const key in outputs) {
-    core.info(`${key}: ${outputs[key]}`)
+    return diff;
+  } catch (error) {
+    core.info("Most likely invalid URL");
+    core.setFailed(error.message);
   }
-  core.endGroup()
 }
+
+async function buildFile(url, addition) {
+  let search, content;
+  core.info(1)
+  const regex = /<body>(.*[\s\S]*)<\/body>/gm;
+  core.info(2)
+  try {
+    const { data } = await axios.get(url);
+    core.info(3)
+    const html = cheerio.load(data).html();
+    core.info("HTML")
+    core.info(html)
+    core.info("----------")
+    while ((search = regex.exec(html)) !== null) {
+      if (search.index === regex.lastIndex) regex.lastIndex++;
+      search.forEach((match, groupIndex) => {
+        content = match;
+        core.info("CONTENT:  ");
+        core.info(content)
+        core.info("-------")
+      });
+    }
+    content = content + addition;
+  } catch (error) {
+    core.info("Most likely invalid URL");
+    core.setFailed(error.message);
+  }
+  return content
+}
+
+autoMerge();
--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const { Base64 } = require("js-base64");
-const axios = require("axios");
-const cheerio = require("cheerio");
 
 async function autoMerge() {
   try {
     const payload = github.context.payload;
-    const filepath = core.getInput("filepath");
-    const raw_link = core.getInput("raw_link");
-    const email = core.getInput("email");
     const myToken = core.getInput("github-token");
-    const octokit = github.getOctokit(myToken).plugin(require("octokit-commit-multiple-files"));
+    const octokit = github.getOctokit(myToken);
 
     const owner = payload.issue.user.login;
     const repo = payload.repository.name;
@@ -23,10 +17,6 @@ async function autoMerge() {
       pull_number: prNumber
     });
 
-    core.info(JSON.stringify(pr))
-    core.setOutput("ref", pr.data.head.ref);
-    core.setOutput("owner", owner);
-    const sha = pr.data.base.sha;
     const mergeable = pr.data.mergeable_state;
     const onlyOneChangedFile = pr.data.changed_files === 1;
     const additions = pr.data.additions;
@@ -38,84 +28,9 @@ async function autoMerge() {
       return;
     };
 
-    // TODO: Fix "dirty" PRs
-
     if (mergeable == "dirty") {
-      // TODO: take care of merge conflicts?
-      core.info("DIRTY")
-      dirty = true;
-      core.setOutput("dirty", dirty);
-      const diffURL = pr.data.diff_url;
-      const diff = await getDiff(diffURL);
-
-      core.info(diff);
-      core.setOutput("diff", diff);
-
-      core.info("BUILD FILE")
-      const content = await buildFile(raw_link, diff);
-      core.info(content);
-      core.info("CONTENT CREATED")
-      const contentEncoded = Base64.encode(content);
-      core.info("CONTENT ENCODED")
-
-      // TODO: Error: Cannot read property 'createOrUpdateFileContents' of undefined
-      core.info("REST")
-      const allRepos = await octokit.rest.repos
-      core.info(JSON.stringify(allRepos))
-      const { data } = allRepos
-      core.info(JSON.stringify(data))
-      core.info("---")
-
-      
-
-      try {
-        // await octokit.rest.repos.createOrUpdateFileContents({
-        //   repo: repo,
-        //   path: filepath,
-        //   message: `Update location.txt with ${owner}'s hometown`,
-        //   content: contentEncoded,
-        //   committer: {
-        //     name: "GitHub-Actions",
-        //     email: email,
-        //   },
-        //   author: {
-        //     name: owner,
-        //     email: email,
-        //   }
-        // })
-        // await octokit.request('PUT /repos/maralihart/test-repo/contents/test.txt', {
-        //   owner: owner,
-        //   repo: repo,
-        //   path: 'test.txt',
-        //   message: 'solving dirty pr',
-        //   content: contentEncoded,
-        //   sha: sha,
-        // })
-        const branchName = await octokit.repos.createOrUpdateFiles({
-          owner: "maralihart",
-          repo: "test-repo",
-          branch: "main",
-          createBranch: true,
-          changes: [
-            {
-              message: "Your commit message",
-              files: {
-                "test.md": `# This is a test`,
-                "test2.md": {
-                  contents: `Something else`,
-                },
-              },
-            },
-          ],
-        });
-        core.info("Successfully updated file");
-      } catch (error) {
-        core.setFailed(error.message);
-      }
-      
-      core.info("Cannot automatically merge this branch");
-      return;
-    };
+      core.info("PR cannot be automatically merged.")
+    }
 
     if (onlyOneChangedFile && oneLineAdded) {
       try {
@@ -134,52 +49,6 @@ async function autoMerge() {
   } catch (error) {
     core.setFailed(error.message);
   }
-}
-
-async function getDiff(url) {
-  let search, diff;
-  const regex = /\+([a-zA-Z]+.*)/gm;
-  try {
-    const { data } = await axios.get(url);
-    const html = cheerio.load(data).html();
-    while ((search = regex.exec(html)) !== null) {
-      if (search.index === regex.lastIndex) regex.lastIndex++;
-      search.forEach((match, groupIndex) => {diff = match;});
-    }
-    return diff;
-  } catch (error) {
-    core.info("Most likely invalid URL");
-    core.setFailed(error.message);
-  }
-}
-
-async function buildFile(url, addition) {
-  let search, content;
-  core.info(1)
-  const regex = /<body>(.*[\s\S]*)<\/body>/gm;
-  core.info(2)
-  try {
-    const { data } = await axios.get(url);
-    core.info(3)
-    const html = cheerio.load(data).html();
-    core.info("HTML")
-    core.info(html)
-    core.info("----------")
-    while ((search = regex.exec(html)) !== null) {
-      if (search.index === regex.lastIndex) regex.lastIndex++;
-      search.forEach((match, groupIndex) => {
-        content = match;
-        core.info("CONTENT:  ");
-        core.info(content)
-        core.info("-------")
-      });
-    }
-    content = content + addition;
-  } catch (error) {
-    core.info("Most likely invalid URL");
-    core.setFailed(error.message);
-  }
-  return content
 }
 
 autoMerge();
